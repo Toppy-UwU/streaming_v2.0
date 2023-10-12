2db4cf9f0142a7c99d4705f4cb89d704d736f6f1
Adithep P <102387194+snxwz@users.noreply.github.com>
2023-10-12 10:37:13 +0700

    New

--- src/page/VideoPage.js
+++ src/page/VideoPage.js
@@ -1,57 +1,121 @@
 import Sidebar from '../components/sidebar';
 import { useEffect, useState } from 'react';
-import { getlocalData } from "../components/session"
-import '../config';
+import { getlocalData, getToken } from "../components/session";
 import moment from 'moment';
 import DataTable, { createTheme, Media } from "react-data-table-component";
 import { Link } from 'react-router-dom';
-import VideoUpdateModal from '../components/videoUpdateModal';
+import Swal from 'sweetalert2';
 
-const Home = () => {
+const UserVideos = () => {
   const [videos, setVideos] = useState([]);
   const [search, setSearch] = useState('');
-  const [filter, setFilter] = useState([])
+  const [filter, setFilter] = useState([]);
+
   document.title = "CS-HUB";
   const ip = global.config.ip.ip;
-  var session = getlocalData('session');
-  const getUserVideo = ip + '/getVideos/profile?u=' + session.U_id + '&p=all';
-
+  const session = getlocalData('session');
+  const getUserVideo = `${ip}/getVideos/profile?u=${session.U_id}&p=all`;
+  const clearApi = ip + '/delete/video/user';
   useEffect(() => {
-    fetch(getUserVideo)
-      .then(response => response.json())
-      .then(data => {
-        setVideos(data);
-        setFilter(data);
-      })
-      .catch(() => { });
-  }, [getUserVideo])
+    const fetchData = async () => {
+      try {
+        const response = await fetch(getUserVideo);
+        if (response.ok) {
+          const data = await response.json();
+          setVideos(data);
+          setFilter(data);
+        } else {
+          console.error('Failed to fetch data');
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
   useEffect(() => {
     const result = videos.filter((item) => {
-      return item.V_title.toLowerCase().match(search.toLocaleLowerCase());
+      return item.V_title.toLowerCase().includes(search.toLowerCase());
     });
     setFilter(result);
   }, [search]);
 
+  const handleClear = async (U_id, U_folder, V_encode) => {
+    const tmp = {
+      'U_id': U_id,
+      'U_folder': U_folder,
+      'V_encode': V_encode
+    }
+
+    try {
+      const response = await fetch(clearApi, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${getToken()}`
+        },
+        body: JSON.stringify(tmp)
+      });
+
+      if (!response.ok) {
+        throw new Error('Clearing failed');
+      }
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  const handleBtn = (U_id, U_folder, V_encode) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "Your video will be deleted!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Delete'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        handleClear(U_id, U_folder, V_encode);
+        Swal.fire({
+          title: 'Deleted!',
+          text: 'Your video has been deleted.',
+          icon: 'success',
+          confirmButtonText: 'OK'
+        }).then(() => {
+          window.location.reload();
+        });
+      }
+    });
+  }
+
   const columns = [
     {
       name: 'Video',
-      selector: row => <img height={120} width={160} style={{ borderRadius: "10px" }} src={`data:image/jpeg;base64, ${row.V_pic}`} alt={row.V_title + " Picture"} />,
-      hide: Media.SM
+      selector: row => row.V_pic,
+      cell: (row) => (
+        <img height={120} width={160} style={{ borderRadius: "10px" }} src={`data:image/jpeg;base64, ${row.V_pic}`} alt={`${row.V_title} Picture`} />
+      ),
+      hide: Media.SM,
+      hide: Media.MD
     },
     {
       name: 'Title',
       selector: row => row.V_title,
-      sortable: true
+      sortable: true,
+      cell: (row) => <Link className="text-decoration-none text-white" to={`/watch?u=${row.U_folder}&v=${row.V_encode}`}>{row.V_title}</Link>
     },
     {
       name: 'Permit',
-      selector: row => row.V_permit.toUpperCase(),
+      selector: row => row.V_permit,
       sortable: true,
     },
     {
       name: 'Date',
-      selector: row => <span>{moment.utc(row.V_upload).format("DD MMMM YYYY")}</span>,
+      selector: row => row.V_upload,
+      cell: (row) => <span>{moment.utc(row.V_upload).format("DD MMMM YYYY")}</span>,
       sortable: true,
       hide: Media.SM
     },
@@ -76,17 +140,12 @@ const Home = () => {
           </button>
           <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby={`dropdown-${row.id}`}>
             <li>
-              <Link className="dropdown-item" to={'/watch?u=' + row.U_folder + '&v=' + row.V_encode}>
+              <Link className="dropdown-item" to={`/watch?u=${row.U_folder}&v=${row.V_encode}`}>
                 <span><i className="bi bi-play-btn"></i> View</span>
               </Link>
             </li>
             <li>
-              <Link className="dropdown-item" to="#" data-bs-toggle="modal" data-bs-target="#UpdateVideoModal">
-                <span><i className="bi bi-gear"></i> Setting</span>
-              </Link>
-            </li>
-            <li>
-              <Link className="dropdown-item" to="#">
+              <Link className="dropdown-item" to="#" onClick={() => handleBtn(session.U_id, row.U_folder, row.V_encode)}>
                 <span><i className="bi bi-trash3"></i> Delete</span>
               </Link>
             </li>
@@ -94,8 +153,7 @@ const Home = () => {
         </div>
       ),
     }
-
-  ]
+  ];
 
   const tableHeaderStyle = {
     headCells: {
@@ -109,7 +167,7 @@ const Home = () => {
         fontSize: "16px",
       }
     },
-  }
+  };
 
   createTheme('solarized', {
     text: {
@@ -132,47 +190,61 @@ const Home = () => {
     },
   }, 'dark');
 
-  return (
-
-    <div>
-      <Sidebar>
-        <div className='container-fluid'>
-          <br />
-          <div className='PageTitle'>
-            <h2><i className="bi bi-collection-play-fill"></i> {session.username} Videos</h2>
-          </div>
-
-          <div className='user-table'>
-            <div className="card">
-              <div className="card-body">
-                <DataTable
-                  customStyles={tableHeaderStyle}
-                  columns={columns}
-                  data={filter}
-                  pagination
-                  fixedHeader
-                  highlightOnHover
-                  theme="solarized"
-                  subHeader
-                  subHeaderComponent={
-                    <input type="text"
-                      className="w-25 form-control"
-                      placeholder="Search..."
-                      value={search}
-                      onChange={(e) => setSearch(e.target.value)}
-                    />
-                  }
-                ></DataTable>
-              </div>
-            </div>
+  if (videos.length > 0) {
+    return (
+      <div>
+        <Sidebar>
+          <div className='container-fluid'>
             <br />
+            <div className='PageTitle'>
+              <h2><i className="bi bi-collection-play-fill"></i> {session.username} Videos</h2>
+            </div>
+            <div className='user-table'>
+              <div className="card">
+                <div className="card-body">
+                  <DataTable
+                    customStyles={tableHeaderStyle}
+                    columns={columns}
+                    data={filter}
+                    pagination
+                    fixedHeader
+                    highlightOnHover
+                    theme="solarized"
+                    subHeader
+                    subHeaderComponent={
+                      <input type="text"
+                        className="w-25 form-control"
+                        placeholder="Search..."
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                      />
+                    }
+                  ></DataTable>
+                </div>
+              </div>
+              <br />
+            </div>
           </div>
-          <VideoUpdateModal id={videos.U_ID} V_id={videos.V_ID} desc={videos.V_desc} title={videos.V_title} permit={videos.V_permit} path={videos.U_folder} encode={videos.V_encode} tags={videos.V_title} />
-        </div>
-      </Sidebar>
-    </div>
-
-  );
+        </Sidebar>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Sidebar>
+          <div className='container-fluid'>
+            <br />
+            <div className='PageTitle'>
+              <h2><i className="bi bi-collection-play-fill"></i> {session.username} Videos</h2>
+            </div>
+            <div className='user-table'>
+              <p>No videos found.</p>
+            </div>
+          </div>
+        </Sidebar>
+      </div>
+    );
+  }
 };
 
-export default Home;
+export default UserVideos;
